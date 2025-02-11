b887a3b89b1ff53600775a2582ef9740ca2e285c
SNARF3 <snarfeo@gmail.com>
2025-02-11 00:42:50 -0400

    Avance nodos

---
+++ Redes/src/Pizarra.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Nodo from "./Nodo";
+import Swal from "sweetalert2"
+const Pizarra = () => {
+    const [nodos, setNodos] = useState([]);
+
+    const solicitarNombre = async () => {
+        const { value: nombre } = await Swal.fire({
+            title: "Ingrese el nombre del nodo",
+            input: "text",
+            inputPlaceholder: "Ejemplo: Nodo A",
+            showCancelButton: true,
+            confirmButtonText: "Aceptar",
+            cancelButtonText: "Cancelar",
+        });
+
+        return nombre || null; // Si cancela, retorna null
+    };
+
+    const handleDoubleClick = async (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+        const nombre = await solicitarNombre();
+        if (!nombre) return; 
+        console.log("nombre: ", nombre)
+        setNodos([...nodos, { x, y, valor: nombre}]);
+    };
+    
+
+    return (
+        <div
+            style={{ width: "100vw", height: "100vh", background: "#112230", position: "relative" }}
+            onDoubleClick={handleDoubleClick}
+        >
+            {nodos.map((nodo) => (
+                <Nodo key={nodo.valor} x={nodo.x} y={nodo.y} valor={nodo.valor}/>
+            ))}
+        </div>
+    );
+};
+
+
+export default Pizarra;
--- Redes/src/main.jsx
+++ Redes/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Nodo from "./Nodo";
+import Pizarra from "./Pizarra.jsx";
 import "./index.css"; // Si usas Tailwind o estilos globales
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Nodo />
+    <Pizarra/>
   </React.StrictMode>
 );
